09a8a1ca6a9e244bd81fa7a3082c87f8f7843660
Artem Zhdanov <72006183+zhdanovan@users.noreply.github.com>
2021-05-16 17:23:26 +0700

    Update and rename Sand.py to Box.py

---
+++ Box.py
@@ -0,0 +1,15 @@
+import pygame
+from settings import *
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.box
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((TEXTURES_SIZE, TEXTURES_SIZE))
+        self.image = pygame.image.load("box.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TEXTURES_SIZE
+        self.rect.y = y * TEXTURES_SIZE
