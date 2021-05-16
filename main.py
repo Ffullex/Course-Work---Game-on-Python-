830d4955f7f1964d949d2ce82dfad25a7ee8b530
Artem Zhdanov <72006183+zhdanovan@users.noreply.github.com>
2021-05-16 17:23:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
+
+import pygame 
 import sys
-from sys import path
-
-import pygame as pg
-from pygame.examples.textinput import BGCOLOR
-
-import Player
-from Sand import Sand
+from os import path
 from settings import *
+from Player import *
+from Box import *
 
 
-class Game():
+class Game:
     def __init__(self):
-        pg.init()
-        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        pygame.key.set_repeat(500, 100)
+        self.set_data()
 
-    # Метод загружает файлы
-    def load_files(self):
-        game_folder = path.dirname(__file__)
-        self.world_dir = []
-        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+    def set_data(self):
+        pg_directory = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(pg_directory, 'map.txt'), 'rt') as f:
             for line in f:
-                self.world_dir.append(line)
+                self.map_data.append(line)
 
-    # Метод загружает спрайты
     def new(self):
-        self.all_sprites = pg.sprite.Group()
-        self.tiles = pg.sprite.Group()
-        for row, blocks in enumerate(self.world_dir):
-            for col, block in enumerate(blocks):
-                if block == '0':
-                    Sand(self, col, row)
-                if block == '1':
-                    self.player = Player(self, col, row)
-                if block == '2':
+        self.all_sprites = pygame.sprite.Group()
+        self.box = pygame.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Box(self, col, row)
+                if tile == '$':
                     self.player = Player(self, col, row)
 
-    # Метод поддерживает игровой цикл
     def run(self):
         self.playing = True
         while self.playing:
@@ -47,69 +41,42 @@ class Game():
             self.update()
             self.draw()
 
-    # Метод выходит из игрового цикла
     def quit(self):
-        pg.quit()
+        pygame.quit()
         sys.exit()
 
-    # Метод обновляет спрайты
     def update(self):
         self.all_sprites.update()
 
-    # Метод создаёт поле
-    def create_field(self):
-        for x in range(0, SCREEN_WIDTH, TEXTURES_SIZE):
-            pg.draw.line(self.screen, SAND_COLOR, (x, 0), (x, TEXTURES_SIZE))
-        for y in range(0, SCREEN_HEIGHT, TEXTURES_SIZE):
-            pg.draw.line(self.screen, SAND_COLOR, (0, y), (TEXTURES_SIZE, y))
+    def create_dungeon(self):
+        for x in range(0, WIDTH, TEXTURES_SIZE):
+            pygame.draw.line(self.screen, SAND_COLOR, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TEXTURES_SIZE):
+            pygame.draw.line(self.screen, SAND_COLOR, (0, y), (WIDTH, y))
 
-    # Метод отрисовывет спрайты и переворачивает дисплей
     def draw(self):
-        self.screen.fill(BGCOLOR)
-        self.create_cell()
+        self.screen.fill(SAND_COLOR)
+        self.create_dungeon()
         self.all_sprites.draw(self.screen)
-        pg.display.flip()
+        pygame.display.flip()
 
-    # Метод обработки событий
     def events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 self.quit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     self.quit()
+                if event.key == pygame.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pygame.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pygame.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pygame.K_DOWN:
+                    self.player.move(dy=1)
 
 g = Game()
 while True:
     g.new()
     g.run()
-#
-# pg.init()
-# win = pg.display.set_mode((520, 520))
-# pg.display.set_caption("Jakob`s Adventures in Dungeon")
-#
-# x = 10
-# y = 10
-#
-# run = True
-# while run:
-#     pg.time.delay(100)
-#
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             run = False
-#
-#     keys = pg.key.get_pressed()
-#     if keys[pg.K_LEFT] and x > 10:
-#         x -= Player.Jakob.speed
-#     if keys[pg.K_RIGHT] and x < 500 - Player.Jakob.width:
-#         x += Player.Jakob.speed
-#     if keys[pg.K_UP] and y > 10:
-#         y -= Player.Jakob.speed
-#     if keys[pg.K_DOWN] and y < 500 - Player.Jakob.height:
-#         y += Player.Jakob.speed
-#
-#     win.fill((0, 0, 0))
-#     pg.draw.rect(win, (0, 0, 255), (x, y, Player.Jakob.width, Player.Jakob.height))
-#     pg.display.update()
-# pg.quit()
