108f12d7889fb09c40b7c474ae12671dbe16d374
Artem Zhdanov <72006183+zhdanovan@users.noreply.github.com>
2021-05-16 17:22:36 +0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,21 +1,29 @@
-import pygame as pg
+import pygame 
 from settings import *
-class Player(pg.sprite.Sprite):
+
+
+class Player(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.image = pg.image.load("img/walk right1.png").convert_alpha()
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((TEXTURES_SIZE, TEXTURES_SIZE))
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
-        self.x = x * WIDTH
-        self.y = y * HEIGHT
+        self.x = x
+        self.y = y
 
-    def Movement(self, x,y):
-        keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT] and x > 10:
-            x -= SPEED
-        if keys[pg.K_RIGHT] and x < 500 - SPEED:
-            x += SPEED
-        if keys[pg.K_UP] and y > 10:
-            y -= SPEED
-        if keys[pg.K_DOWN] and y < 500 - SPEED:
-            y += SPEED
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx, dy):
+            self.x += dx
+            self.y += dy
+
+    def collide_with_walls(self, dx=0, dy=0):
+        for box in self.game.box:
+            if box.x == self.x + dx and box.y == self.y + dy:
+                return True
+        return False
+
+    def update(self):
+        self.rect.x = self.x * TEXTURES_SIZE
+        self.rect.y = self.y * TEXTURES_SIZE
