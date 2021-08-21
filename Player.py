23f2f05a2872305e997d12cc12f6a30a7be5359d
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-08-21 07:13:57 +0700

    Created logic collides with walls && aqua

--- Player.py
+++ Player.py
@@ -3,6 +3,7 @@ from os import path
 import pygame
 import pygame.sprite
 
+import Box
 from Camera import Map
 from settings import *
 
@@ -22,19 +23,24 @@ class Player(pygame.sprite.Sprite):
 
 # Метод - передвижение
     def move(self, dx=0, dy=0):
-        if not self.collide_with_walls(dx, dy):
+        if self.collide_with_walls(dx, dy):
+            self.x = self.x
+            self.y = self.y
+        elif self.collide_with_aqua(dx, dy):
+            self.x = self.x
+            self.y = self.y
+        elif self.collide_with_mark(dx, dy):
             self.x += dx
             self.y += dy
-        elif not self.collide_with_aqua(dx, dy):
-            self.x += dx
-            self.y += dy
-        elif not self.collide_with_mark(dx, dy):
-            self.x += dx
-            self.y += dy
-        elif not self.collide_with_boxes(dx, dy):
+        elif self.collide_with_boxes(dx, dy):
+            # Box.x += dx
+            # Box.y += dy
             self.x += dx
             self.y += dy
         elif self.collide_with_portal(dx, dy):
+            self.x += dx
+            self.y += dy
+        else:
             self.x += dx
             self.y += dy
 
@@ -43,40 +49,40 @@ class Player(pygame.sprite.Sprite):
         for aqua in self.game.aqua:
             if aqua.x == self.x + dx and aqua.y == self.y + dy:
                 print('aqua')
-                return False
-        return True
+                return True
+        return False
 
 # Метод - столкновение со стенами
     def collide_with_walls(self, dx=0, dy=0):
         for wall in self.game.wall:
             if wall.x == self.x + dx and wall.y == self.y + dy:
                 print('wall')
-                return False
-        return True
+                return True
+        return False
 
 # Метод - столкновение с коробками
     def collide_with_boxes(self, dx=0, dy=0):
         for box in self.game.box:
             if box.x == self.x + dx and box.y == self.y + dy:
                 print('box')
-                return False
-        return True
+                return True
+        return False
 
 # Метод - столкновение с порталом
     def collide_with_portal(self, dx=0, dy=0):
         for portal in self.game.portal:
             if portal.x == self.x + dx and portal.y == self.y + dy:
                 print('portal')
-                return False
-        return True
+                return True
+        return False
 
 # Метод - столкновение с пометкой
     def collide_with_mark(self, dx=0, dy=0):
         for mark in self.game.mark:
             if mark.x == self.x + dx and mark.y == self.y + dy:
                 print('mark')
-                return False
-        return True
+                return True
+        return False
 
     def update(self):
         self.rect.x = self.x * TEXTURES_SIZE
